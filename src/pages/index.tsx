8cc0de0ccd9a744b643186d70c3d321851106a30
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-10-13 21:38:42 -0300

    created a simple map

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,36 @@
 import { Hero } from '../components/Hero';
 
+import { useWindowSize } from '../hooks/useWindowSize';
+
 export default function Home() {
+  const { width, height } = useWindowSize();
+
+  const horizontalSquares = Math.round((width || 0) / 84);
+  const verticalSquares = Math.round((height || 0) / 84);
+
+  const horizontalSquaresArray = Array.from(Array(horizontalSquares).keys());
+  const verticalSquaresArray = Array.from(Array(verticalSquares).keys());
+
   return (
     <div
       style={{ backgroundColor: 'green', position: 'relative', height: '100%' }}
     >
-      walk simulator game
       <Hero />
+      {verticalSquaresArray.map(itemVertical => (
+        <div style={{ display: 'flex' }} key={itemVertical}>
+          {horizontalSquaresArray.map(itemHorizontal => (
+            <div
+              style={{
+                width: '84px',
+                height: '84px',
+                background: 'blue',
+                border: '1px solid red',
+              }}
+              key={`${itemVertical}-${itemHorizontal}`}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
