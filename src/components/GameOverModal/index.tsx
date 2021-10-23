9b57adda71c6b79c7cadf4f381e48a8cb850bd4e
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-10-23 18:43:53 -0300

    created high score with cookies

--- src/components/GameOverModal/index.tsx
+++ src/components/GameOverModal/index.tsx
@@ -1,9 +1,11 @@
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import Cookies from 'js-cookie';
 
 import { setIsLoading } from '../../redux/modules/map/actions';
 import { setGameOver, setScore } from '../../redux/modules/score/actions';
 
-import { IScoreState } from '../../redux/modules/score/types';
+import { IScore, IScoreState } from '../../redux/modules/score/types';
 import { IGlobalReduxState } from '../../redux/store';
 
 import {
@@ -19,6 +21,8 @@ export function GameOverModal(): JSX.Element {
   const { score, gameOver } = useSelector<IGlobalReduxState, IScoreState>(
     state => state.scoreReducer,
   );
+  const [highScore, setHighScore] = useState<IScore | undefined>(undefined);
+
   const dispatch = useDispatch();
 
   function handlePlayAggain() {
@@ -32,6 +36,27 @@ export function GameOverModal(): JSX.Element {
     dispatch(setIsLoading(true));
   }
 
+  useEffect(() => {
+    if (gameOver) {
+      const highScoreCookie = Cookies.get('WALK_SIMULATOR_GAME:HIGH_SCORE');
+      const highScoreValue: IScore = highScoreCookie
+        ? JSON.parse(highScoreCookie)
+        : { enemiesKilled: 0, mapsGenerated: 0 };
+
+      if (score.mapsGenerated >= highScoreValue.mapsGenerated) {
+        if (score.enemiesKilled > highScoreValue.enemiesKilled) {
+          Cookies.set('WALK_SIMULATOR_GAME:HIGH_SCORE', JSON.stringify(score), {
+            expires: 365,
+          });
+
+          setHighScore(score);
+        }
+      } else {
+        setHighScore(highScoreValue);
+      }
+    }
+  }, [gameOver, score]);
+
   return (
     <Container isActive={gameOver}>
       <ContentContainer>
@@ -44,8 +69,9 @@ export function GameOverModal(): JSX.Element {
             <strong>{score.enemiesKilled} inimigos derrotados</strong>.
           </ScoreItem>
           <ScoreItem>
-            Melhor Pontuação: <strong>2 mapas gerados</strong> e{' '}
-            <strong>50 inimigos derrotados</strong>.
+            Melhor Pontuação:{' '}
+            <strong>{highScore?.mapsGenerated} mapas gerados</strong> e{' '}
+            <strong>{highScore?.enemiesKilled} inimigos derrotados</strong>.
           </ScoreItem>
         </ScoreContainer>
 
