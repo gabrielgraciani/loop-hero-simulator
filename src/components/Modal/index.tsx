885c6078aa4d1aa64879ce6e127aa27eba5f43c0
Gabriel Graciani <gabrieltgraciani@hotmail.com>
2021-10-23 18:18:21 -0300

    created game over modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,12 +1,58 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setIsLoading } from '../../redux/modules/map/actions';
+import { setGameOver, setScore } from '../../redux/modules/score/actions';
+
 import { IScoreState } from '../../redux/modules/score/types';
 import { IGlobalReduxState } from '../../redux/store';
-import { Container } from './styles';
+
+import {
+  Container,
+  ContentContainer,
+  Title,
+  ScoreContainer,
+  ScoreItem,
+  TryAggainButton,
+} from './styles';
 
 export function Modal(): JSX.Element {
   const { score, gameOver } = useSelector<IGlobalReduxState, IScoreState>(
     state => state.scoreReducer,
   );
+  const dispatch = useDispatch();
 
-  return gameOver ? <Container> VOCÊ MORREU </Container> : <></>;
+  function handlePlayAggain() {
+    dispatch(
+      setScore({
+        mapsGenerated: 0,
+        enemiesKilled: 0,
+      }),
+    );
+    dispatch(setGameOver(false));
+    dispatch(setIsLoading(true));
+  }
+
+  return (
+    <Container isActive={gameOver}>
+      <ContentContainer>
+        <Title>FIM DE JOGO</Title>
+
+        <ScoreContainer>
+          <ScoreItem>
+            Pontuação Atual:{' '}
+            <strong>{score.mapsGenerated} mapas gerados</strong> e{' '}
+            <strong>{score.enemiesKilled} inimigos derrotados</strong>.
+          </ScoreItem>
+          <ScoreItem>
+            Melhor Pontuação: <strong>2 mapas gerados</strong> e{' '}
+            <strong>50 inimigos derrotados</strong>.
+          </ScoreItem>
+        </ScoreContainer>
+
+        <TryAggainButton onClick={handlePlayAggain}>
+          Jogar Novamente
+        </TryAggainButton>
+      </ContentContainer>
+    </Container>
+  );
 }
--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
+
 import { Colors } from '../../styles/Colors';
 
-const Container = styled.div`
+import { IStyledModalProps } from './types';
+
+const Container = styled.div<IStyledModalProps>`
   width: 100%;
   height: 100vh;
   position: fixed;
@@ -12,6 +15,9 @@ const Container = styled.div`
   justify-content: center;
   flex-direction: row;
   z-index: 999;
+  opacity: ${({ isActive }) => (isActive ? '1' : '0')};
+  visibility: ${({ isActive }) => (isActive ? 'visible' : 'hidden')};
+  transition: opacity 0.5 ease;
 
   &:after {
     content: '';
@@ -24,4 +30,45 @@ const Container = styled.div`
   }
 `;
 
-export { Container };
+const ContentContainer = styled.div`
+  background: ${Colors.white};
+  z-index: 9999;
+  border-radius: 0.4rem;
+  padding: 4rem 8rem;
+`;
+
+const Title = styled.h2`
+  text-align: center;
+  color: ${Colors.black};
+`;
+
+const ScoreContainer = styled.div`
+  width: 100%;
+`;
+
+const ScoreItem = styled.h4`
+  width: 100%;
+  color: ${Colors.black};
+  margin-top: 1.2rem;
+
+  strong {
+    color: ${Colors.life};
+  }
+`;
+
+const TryAggainButton = styled.button`
+  margin-top: 2.4rem;
+  width: 100%;
+  background: red;
+  padding: 1rem;
+  border-radius: 0.4rem;
+`;
+
+export {
+  Container,
+  ContentContainer,
+  Title,
+  ScoreContainer,
+  ScoreItem,
+  TryAggainButton,
+};
